e3d4d68603f4605ffb1465c12325933e694933c2
bakkar3 <amarbakar33@gmail.com>
2022-03-12 00:27:47 +0100

    de

--- src/components/test.js
+++
@@ -1,6 +0,0 @@
-let a = 10;
-let b = "20";
-let c = 80;
-console.log(++a + +b++ + +c++ - +a++);
-console.log(++a + -b + +c++ - -a++ + +a);
-console.log(++c + +b + --a * +b++ - +b * a + --a - +true);
